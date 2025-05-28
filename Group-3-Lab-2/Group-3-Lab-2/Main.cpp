45b53b0bb881d8cb5e0852ea9edd5ddaddce334a
AndrewRBurge <burgean@sheridancollege.ca>
2025-05-28 16:01:53 -0400

    set up truck class
    - set up truck class functions
    -tested truck class with data provided
    -saved

--- Group-3-Lab-2/Group-3-Lab-2/Main.cpp
+++ Group-3-Lab-2/Group-3-Lab-2/Main.cpp
@@ -3,10 +3,16 @@
 #include <cmath>
 using namespace std;
 
+#define MAXTRUCKS 5
+#define ZERO 0;
+#define DEFAULT "unassigned"
 
 //test classes
 class driver{
-
+public:
+	string FirstName;
+	string lastName;
+	int id;
 };
 
 class location {
@@ -24,22 +30,45 @@ class Truck {
 public:
 	int id;
 
+	Truck() {
+		d.FirstName = DEFAULT;
+		d.lastName = DEFAULT;
+		d.id = ZERO;
+		id = ZERO;
+		TruckLoc.x = ZERO;
+		TruckLoc.y = ZERO;
+	}
+
+	Truck(string first, string last, int driverID, int i, int x,int y) {
+	
+		d.FirstName = first;
+		d.lastName = last;
+		d.id = driverID; 
+		id = i;
+		TruckLoc.x = x;
+		TruckLoc.y = y;
+
+
+	}
+
 	int Setid(int i) {
 		id = i;
 	}
 
+
+
 	// will calcuated this trucks distance from TruckB
-	int distanceFrom(Truck TruckB) {
-		int z;
-		int a = pow(TruckLoc.x - TruckB.TruckLoc.x, 2);
-		int b = pow(TruckLoc.y - TruckB.TruckLoc.y, 2);
+	double distanceFrom(Truck TruckB) {
+		double z;
+		double a = pow(TruckLoc.x - TruckB.TruckLoc.x, 2);
+		double b = pow(TruckLoc.y - TruckB.TruckLoc.y, 2);
 		z = sqrt(a + b);
 		return (z);
 	}
-	int distanceFrom(location location) {
-		int z;
-		int a = pow(TruckLoc.x - location.x, 2);
-		int b = pow(TruckLoc.y - location.y, 2);
+	double distanceFrom(location location) {
+		double z;
+		double a = pow(TruckLoc.x - location.x, 2);
+		double b = pow(TruckLoc.y - location.y, 2);
 		z = sqrt(a + b);
 		return (z);
 	}
@@ -48,21 +77,24 @@ public:
 
 
 
-void main(){
-	Truck truck[2];
+int main(){
+	//Truck truck[MAXTRUCKS];
 	
-	truck[1].id = 77;
-	cout << "The distance between the two trucks is:";
-	cout << "The distance between the truck (with ID = 178534) and the origin is:";
-	cout << "The distance between the truck (with ID = 245817) and the origin is:";
+	Truck truck1("Liz", "Brian", 35, 178534, 2, 3);
+	Truck truck2("Mike", "Smith", 69, 245817, 5, 2);
+
+	location origin;
+	origin.x = 0;
+	origin.y = 0;
+
+	cout << "The distance between the two trucks is:" << truck1.distanceFrom(truck2) << "\n";
+	cout << "The distance between the truck (with ID = 178534) and the origin is:" << truck1.distanceFrom(origin) << "\n";
+	cout << "The distance between the truck (with ID = 245817) and the origin is:" << truck2.distanceFrom(origin) << "\n";
 
 	/*
 The output of your code must be the three required distances as follows:
 The distance between the two trucks is:?
 The distance between the truck (with ID = 178534) and the origin is: ?
 The distance between the truck (with ID = 245817) and the origin is: ?
-	
-	
-	
 	*/
 }
